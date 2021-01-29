fff1e0cd199f237f4609bbe3d88da93f936c3c18
TianLong <pravashkc3@gmail.com>
2021-01-29 21:36:04 +0800

    connect trello

--- controllers/trelloController.js
+++ controllers/trelloController.js
@@ -1,31 +1,18 @@
-const Trello = require('trello-node-api')(process.env.TRELLO_API_KEY, process.env.TRELLO_OAUTH_TOKEN);
+const fetch = require('node-fetch');
 
 module.exports = {
     createBoard: async (req) => {
         console.log('request', req.name);
-        const data = {
-            name: req.name, // REQUIRED
-            defaultLabels: false,
-            defaultLists: false,
-            desc: 'Board description.',
-            idOrganization: 'ORGANIZATION_ID',
-            idBoardSource: 'BOARD_ID',
-            keepFromSource: 'none',
-            powerUps: 'all',
-            prefs_permissionLevel: 'private',
-            prefs_voting: 'disabled',
-            prefs_comments: 'members',
-            prefs_invitations: 'members',
-            prefs_selfJoin: true,
-            prefs_cardCovers: true,
-            prefs_background: 'blue',
-            prefs_cardAging: 'regular'
-        }
-
-        Trello.board.create(data).then(function (response) {
-            console.log('response ', response);
-        }).catch(function (error) {
-            console.log('error', error);
-        });
+        fetch(`https://api.trello.com/1/boards/?key=${process.env.TRELLO_API_KEY}&token=${process.env.TRELLO_OAUTH_TOKEN}&name=${req.name}`, {
+            method: 'POST'
+        })
+        .then(response => {
+            console.log(
+            `Response: ${response.status} ${response.statusText}`
+            );
+            return response.text();
+        })
+        .then(text => console.log(text))
+        .catch(err => console.error(err));
     }
 }
